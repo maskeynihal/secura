8a3b0ec07e969acc9ac21e935decc0884971bf36
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2024-10-01 17:29:13 +0545

    chore: add script to start vault

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
+read -p "Enter URL for vault: " vault_url
+
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vault
+
+sudo apt install certbot
+
+sudo certbot certonly --standalone -d $vault_url
+
+sudo chown -R vault:vault /etc/letsencrypt
+
+sudo cp ./vault.service /etc/systemd/system/vault.service
+
+sudo cp ./vault.hcl /etc/vault.d/vault.hcl
+
+sudo systemctl reload-daemon
+
+sudo systemctl enable vault
+
+sudo systemctl start vault
